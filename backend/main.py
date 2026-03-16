059cafc7b640fe1f4c96fb25c5dd01a26fc18d18
ernest.prykhodko <ernest.prykhodko@kitrum.com>
2026-03-16 13:37:50 +0200

    fix

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://rag-assistant-llm-python-react-1.onrender.com"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
